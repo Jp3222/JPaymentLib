eb703925c79ecc5934133b5a41799b702480c76c
Jp3222 <Usuario@USUARIO-AFE35JK>
2026-07-28 15:53:55 -0600

    JPCC 2026-07-27 ADD: FUTURA CLASE ORQUESTADORA PARA RECIBIR Y GENERAR PAGOS

---
+++ src/main/java/jsoftware/com/jpaymentlib/model/service/PaymentService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jsoftware.com.jpaymentlib.model.service;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PaymentService {
+
+    public boolean paymentProcess() {
+        boolean res = false;
+        return res;
+    }
+
+    public boolean paymentService() {
+        boolean res = false;
+        return res;
+    }
+}
